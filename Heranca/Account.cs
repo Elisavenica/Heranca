eb237ac126e5bb1a26d45ea52237d671f51a1fbc
Elisa <elisac02@outlook.com>
2025-05-06 16:46:42 -0300

    Adicionar arquivos de projeto.

---
+++ Heranca/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Course.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+        public void withdraw(double amount)
+        {
+
+            Balance -= amount;
+        }
+            public void Deposit(double amount)
+            {
+
+                Balance += amount;
+            }
+
+
+
+        }
+
+    }
+
+
---
+++ Heranca/BusinessAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace Course.Entities;
+
+    internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount > 0 && amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+
